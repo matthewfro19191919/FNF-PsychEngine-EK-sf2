25d827c72557b63aad1121281d4d37dc76f3df33
tposejank <81495861+tposejank@users.noreply.github.com>
2022-08-27 09:47:07 -0500

    Dangit

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -1450,7 +1450,7 @@ class ChartingState extends MusicBeatState
 			}
 			else if (wname == 'mania')
 			{
-				_song.mania = nums.value
+				_song.mania = nums.value;
 				reloadGridLayer();
 			}
 			else if (wname == 'song_bpm')
