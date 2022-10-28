759023582677529b5f9ce3e2e63f53ffbc6772eb
tposejank <81495861+tposejank@users.noreply.github.com>
2022-10-28 18:23:35 -0500

    a

--- source/notes/NoteSplash.hx
+++ source/notes/NoteSplash.hx
@@ -12,6 +12,8 @@ class NoteSplash extends FlxSprite
 	private var idleAnim:String;
 	private var textureLoaded:String = null;
 
+	public var strumPos:Array<Float> = [0, 0];
+
 	public function new(x:Float = 0, y:Float = 0, ?note:Int = 0) {
 		super(x, y);
 
@@ -28,15 +30,17 @@ class NoteSplash extends FlxSprite
 	}
 
 	public function setupNoteSplash(x:Float, y:Float, note:Int = 0, texture:String = null, hueColor:Float = 0, satColor:Float = 0, brtColor:Float = 0) {
+		strumPos = [x, y];
+		
 		setPosition(x - Note.swagWidth * 0.95, y - Note.swagWidth);
 		alpha = 0.6;
 
 		if(texture == null) {
 			texture = 'noteSplashes';
-			if(PlayState.SONG.splashSkin != null && PlayState.SONG.splashSkin.length > 0) texture = PlayState.SONG.splashSkin;
 			if (PlayState.isPixelStage) {
 				texture += '-pixel';
 			}
+			if(PlayState.SONG.splashSkin != null && PlayState.SONG.splashSkin.length > 0) texture = PlayState.SONG.splashSkin;
 		}
 
 		if(textureLoaded != texture) {
@@ -45,12 +49,12 @@ class NoteSplash extends FlxSprite
 		colorSwap.hue = hueColor;
 		colorSwap.saturation = satColor;
 		colorSwap.brightness = brtColor;
-		
-		offset.set(10, 10);
+
+		doOffset();
 
 		var animNum:Int = FlxG.random.int(1, 2);
 		animation.play('note' + note + '-' + animNum, true);
-		if(animation.curAnim != null)animation.curAnim.frameRate = 24 + FlxG.random.int(-2, 2);
+		if(animation.curAnim != null)animation.curAnim.frameRate += FlxG.random.int(-2, 2);
 	}
 
 	function loadAnims(skin:String) {
@@ -73,6 +77,15 @@ class NoteSplash extends FlxSprite
             antialiasing = false;
             setGraphicSize(Std.int(width * PlayState.daPixelZoom));
             updateHitbox();
+		}
+	}
+
+	function doOffset() {
+		if (PlayState.isPixelStage) {
+			var hsw = Note.swagWidth * 0.5;
+			setPosition(strumPos[0] - (width / 2) + hsw, strumPos[1] - (height / 2) + hsw);
+		} else {
+			offset.set(10, 10);
 		}
 	}
 
