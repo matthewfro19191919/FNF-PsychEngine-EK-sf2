8cbebb44c9a686cedbb74e4fe6d4fd11dd435315
tposejank <81495861+tposejank@users.noreply.github.com>
2024-08-10 16:19:32 -0400

    exclamation mark!
    haha

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1619,7 +1619,7 @@ class PlayState extends MusicBeatState
 		adaptStrumline(opponentStrums);
 		adaptStrumline(playerStrums);
 
-		if (controls.mobileC) {
+		if (!controls.mobileC) {
 			for (i in 0...playerStrums.members.length) {
 				var keyShowcase = new KeybindShowcase(
 					playerStrums.members[i].x, 
